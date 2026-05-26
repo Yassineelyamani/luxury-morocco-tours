82b351045e24e1cad4526da2116504ecbba8a575
Yassineelyamani <101007420+Yassineelyamani@users.noreply.github.com>
2026-05-25 23:27:41 -0700

    Update admin.ts

--- client/lib/firebase/admin.ts
+++ client/lib/firebase/admin.ts
@@ -5,11 +5,9 @@ import { getAuth } from 'firebase-admin/auth'
 if (!getApps().length) {
   initializeApp({
     credential: cert({
-     projectId: process.env.FIREBASE_PROJECT_ID || "dummy-project",
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL || "dummy@email.com",
-      privateKey: process.env.FIREBASE_PRIVATE_KEY
-  ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
-  : undefined,
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
     }),
   })
 }
